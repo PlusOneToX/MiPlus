a92e8c53c85a5879a8acf5a22ce27b69e42df2a7
YJXS <353667349@qq.com>
2021-06-15 11:26:26 +0800

    MiPlus register function OK

--- nodejs/router/mi.js
+++ nodejs/router/mi.js
@@ -20,6 +20,36 @@ r.get("/login/:miname&:mipwd", (req, res) => {
     });
 });
 
+//注册用户
+r.post("/register", (req, res) => {
+    var obj = req.body;
+    var sql = "INSERT INTO miuser (miid,miname,miphone,mipwd) VALUES(null,?,?,?)";
+    console.log(obj);
+    pool.query(sql, [obj.miname, obj.miphone, obj.mipwd], (err, result) => {
+        if (err) throw err;
+        if (result.affectedRows > 0) {
+            // res.sendStatus(201);
+            res.send("1");
+        } else {
+            res.send("0");
+        }
+    });
+});
+
+//判断用户名是否存在
+r.get("/register/isexists", (req, res) => {
+    var obj = req.query;
+    var sql = "SELECT miid FROM miuser WHERE miphone = ?";
+    console.log(sql, obj);
+    pool.query(sql, [obj.miphone], (err, result) => {
+        if (err) throw err;
+        if (result.length > 0) {
+            res.send("1");
+        } else {
+            res.send("0");
+        }
+    });
+});
 
 //导出路由器对象
 module.exports = r;
